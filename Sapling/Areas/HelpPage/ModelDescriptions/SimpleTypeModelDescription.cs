f84e1d143faba9331ebb3625e677c14f8646a5e6
Suresh G <Krisurnok@gmail.com>
2017-08-15 20:54:13 +0530

    Revert "Initial setup"
    This reverts commit 401af74127165ba22f20b0fa69271c1676489b3b.

--- Sapling/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Sapling.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Sapling/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sapling.WebApiApplication" Language="C#" %>
