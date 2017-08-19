539c20f581ffa47118144d48b270e7fff40bc2ac
Dhilipfr <dhilipfr@gmail.com>
2017-08-19 15:53:37 +0530

    sapling bl

---
+++ Sapling.BL/BLSapling.cs
@@ -0,0 +1,24 @@
+﻿using Sapling.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapling.BL
+{
+    internal class BLSapling
+    {
+        public static void GetSaplingsLocations()
+        {
+            try
+            {
+               
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
